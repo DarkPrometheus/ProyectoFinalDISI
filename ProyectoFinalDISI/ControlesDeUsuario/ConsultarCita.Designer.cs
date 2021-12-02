dddc207e40e91484f688a39da61aac489c87c1d0
crisarael <crisarael@gmail.com>
2021-12-02 15:26:16 -0800

    combo box vinculados a la base de datos

--- ProyectoFinalDISI/ControlesDeUsuario/ConsultarCita.Designer.cs
+++ ProyectoFinalDISI/ControlesDeUsuario/ConsultarCita.Designer.cs
@@ -169,7 +169,7 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(93, 158);
+            this.dataGridView1.Location = new System.Drawing.Point(68, 165);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(598, 266);
             this.dataGridView1.TabIndex = 30;
